17be9991c008cddf352220bc22217531699968f8
howl howl <github.casually337@passinbox.com>
2026-04-12 21:37:08 +0100

    feat(ui): mejorar formulario de contacto y validación de campos style(ui): ajustar estilos de encabezado y pie de página fix(ui): corregir enlace en la sección de proyectos

--- js/responsiveMenu.js
+++ js/responsiveMenu.js
@@ -1,7 +1,6 @@
 const menuBtn = document.getElementById("mobileBtn");
 const menuMobile = document.getElementById("mobileNav");
 
-console.log("Script cargado");
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("active");
 });
