3e1cb12931cd0c862286afabf8eac18e146f1672
howl howl <github.casually337@passinbox.com>
2026-08-08 19:54:12 +0100

    feat: añadir endpoint GET para obtener tecnologías

--- backend/src/controller/technology.js
+++ backend/src/controller/technology.js
@@ -4,6 +4,21 @@ import { requireAdmin } from "../middleware/middleware.js";
 
 const technologyRouter = Router();
 
+technologyRouter.get("/", async (req, res) => {
+  const prisma = createPrisma();
+  try {
+    const technologies = await prisma.technology.findMany();
+
+    res.status(200).json(technologies);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "No se pudieron obtener las tecnologias.",
+    });
+  }
+});
+
 technologyRouter.post("/", requireAdmin, async (req, res) => {
   const prisma = createPrisma();
   const { name, slug } = req.body;
